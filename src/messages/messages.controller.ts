151230141eceb06a6044cf073e29de3b37685fe2
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-10-14 13:44:05 +0100

    import functionality into the controller methods for respective requests

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,20 +1,29 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessageService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+    messagesService: MessageService;
+
+    constructor() {
+        // * Dont do this in a real app!
+        // * Use Dependency Injection
+        this.messagesService = new MessageService();
+    }
+
     @Get()
     listMessages() {
-        
+        return this.messagesService.findAll();
     }
 
     @Post()
     createMessage(@Body() body: CreateMessageDto) {
-        console.log(body);
+        return this.messagesService.create(body.content);
     }
 
     @Get('/:id')
     getMessage(@Param('id') id: string) {
-        console.log(id);
+        return this.messagesService.findOne(id);
     }
 }
