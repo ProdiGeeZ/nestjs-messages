25db50adf202148a30b0021907e957bcf8686d9b
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-10-14 13:25:16 +0100

    Create methods for interacting with storage in repository

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,28 @@
+import { readFile, writeFile } from "fs/promises";
+
+export class MessagesRepository {
+    async findOne(id: string) {
+        const contents = await readFile('messages.json', 'utf-8');
+        const messages = JSON.parse(contents);
+        return messages[id];
+    }
+
+    async findAll() {
+        const contents = await readFile('messages.json', 'utf-8');
+        const messages = JSON.parse(contents);
+        return messages;
+    }
+
+    async create(content: string) {
+        const contents = await readFile('messages.json', 'utf-8');
+        const messages = JSON.parse(contents);
+
+        const id = Math.max(...Object.keys(messages).map(key => parseInt(key))) + 1;
+        const newMessage = { id, content };
+
+        messages[id] = newMessage;
+
+        await writeFile('messages.json', JSON.stringify(messages, null, 2));
+        return newMessage;
+    }
+}
